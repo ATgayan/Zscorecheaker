250698536a457a331112f6a7e824cbad90329dc1
dilshancoder <146836586+dilshancoder@users.noreply.github.com>
2024-01-17 20:15:05 +0530

    set loading

--- client/src/component/pages/home.jsx
+++ client/src/component/pages/home.jsx
@@ -1,29 +1,46 @@
 import React from "react";
 import Mnavbar from "./navbar";
-import image from '../images/image2.png'
-import "../styles/homepage.css"
-import { Link } from "react-router-dom";
-
+import image from '../images/image2.png';
+import "../styles/homepage.css";
+import { useNavigate } from "react-router-dom";
 
 function Homepage() {
-  
+  const [loading, setLoading] = React.useState(false);
+  const navigate = useNavigate();
+
+  const handleLoading = () => {
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+      navigate('/Resultpage');
+    }, 1000);
+  };
+
   return (
     <div>
-      <Mnavbar></Mnavbar>
+      <Mnavbar />
       <div className="container">
-      <div className="Text">
-        <h1>Find Your Best <span>Course</span><br/> On your Z-Score</h1>
-         <div className="subtext">
-           <p>Minimum "Z" Scores for selection to various Courses of Study of Universities under the Normal Intake, in respect of each district</p>
-         </div>
-          <Link to={'/Resultpage'} ><button className="homebtn">Find Course</button></Link>
-      </div>
+        <div className="Text">
+          <h1>
+            Find Your Best <span>Course</span>
+            <br /> On your Z-Score
+          </h1>
+          <div className="subtext">
+            <p>
+              Minimum "Z" Scores for selection to various Courses of Study of
+              Universities under the Normal Intake, in respect of each district
+            </p>
+          </div>
+          <button onClick={handleLoading} className="homebtn">
+            {loading ? "Loading..." : "Find Course"}
+          </button>
+        </div>
 
-      <div className="imgclass">
-        <img src={image} alt="Logo"/>;
+        <div className="imgclass">
+          <img src={image} alt="Logo" />;
+        </div>
       </div>
-
-    </div>
     </div>
   );
 }
